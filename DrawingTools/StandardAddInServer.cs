9413bc89e2f106b20dcfb72439768281c2b32106
tenthdoctor301 <139527335+tenthdoctor301@users.noreply.github.com>
2023-07-18 11:39:07 +0700

    Div 002

--- DrawingTools/StandardAddInServer.cs
+++ DrawingTools/StandardAddInServer.cs
@@ -2,9 +2,13 @@ using Inventor;
 using Microsoft.Win32;
 using System;
 using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Windows.Forms;
+
 
 namespace DrawingTools
 {
+    
     /// <summary>
     /// This is the primary AddIn Server class that implements the ApplicationAddInServer interface
     /// that all Inventor AddIns are required to implement. The communication between Inventor and
@@ -15,14 +19,16 @@ namespace DrawingTools
     {
 
         // Inventor application object.
-        private Inventor.Application m_inventorApplication;
-
+        Inventor.Application m_inventorApplication;
+        ButtonDefinition CreateDetailsButton;
+        //UserInterfaceEvents m_uiEvents;
+        //ApplicationEvents m_appEvents;
+        private ButtonDefinitionSink_OnExecuteEventHandler ButtonDefinition_OnExecuteEventDelegate;
         public StandardAddInServer()
         {
         }
 
         #region ApplicationAddInServer Members
-
         public void Activate(Inventor.ApplicationAddInSite addInSiteObject, bool firstTime)
         {
             // This method is called by Inventor when it loads the addin.
@@ -31,11 +37,27 @@ namespace DrawingTools
 
             // Initialize AddIn members.
             m_inventorApplication = addInSiteObject.Application;
+            //m_uiEvents = m_inventorApplication.UserInterfaceManager.UserInterfaceEvents;
+            //m_appEvents = m_inventorApplication.ApplicationEvents;
+            
+            // creating a button definition.
+            stdole.IPictureDisp largeIcon = PictureDispConverter.ToIPictureDisp(InvAddIn.Properties.Resources._32x32);
+            stdole.IPictureDisp smallIcon = PictureDispConverter.ToIPictureDisp(InvAddIn.Properties.Resources._16x16);
+            ControlDefinitions controlDefs = m_inventorApplication.CommandManager.ControlDefinitions;
+            CreateDetailsButton = controlDefs.AddButtonDefinition("View Details", "id_viewdetails_bt", CommandTypesEnum.kShapeEditCmdType, AddInClientID(), "", "", smallIcon, largeIcon);
+            ButtonDefinition_OnExecuteEventDelegate = new ButtonDefinitionSink_OnExecuteEventHandler(this.CreateDetailsButton_OnExecute);
+            CreateDetailsButton.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
 
+            // Add to the user interface, if it's the first time.
+            if (firstTime == true)
+            {
+                AddToUserInterface();
+            }
+            
             // TODO: Add ApplicationAddInServer.Activate implementation.
             // e.g. event initialization, command creation etc.
         }
-
+        
         public void Deactivate()
         {
             // This method is called by Inventor when the AddIn is unloaded.
@@ -70,7 +92,94 @@ namespace DrawingTools
                 return null;
             }
         }
+       
+        public void CreateDetailsButton_OnExecute(NameValueMap context)
+        {
+            invTools.PartDetails r = new invTools.PartDetails();
+            r.ViewDetails();
+            
+        }
 
+        private void AddToUserInterface()
+        {
+            RibbonPanel panel;
+            try
+            {
+                panel = m_inventorApplication.UserInterfaceManager.Ribbons["Drawing"].RibbonTabs["id_TabPlaceViews"].RibbonPanels["id_UserToolTab"];
+            }
+            catch
+            {
+                panel = m_inventorApplication.UserInterfaceManager.Ribbons["Drawing"].RibbonTabs["id_TabPlaceViews"].RibbonPanels.Add("Tool Panel", "id_UserToolTab", AddInClientID(), "",false);
+            }
+            panel.CommandControls.AddButton(CreateDetailsButton);
+        }
+
+        public string AddInClientID()
+        {
+            GuidAttribute addInCLSID;
+            addInCLSID = (GuidAttribute)GuidAttribute.GetCustomAttribute(typeof(StandardAddInServer), typeof(GuidAttribute));
+            string addInCLSIDString;
+            addInCLSIDString = "{" + addInCLSID.Value + "}";
+            return addInCLSIDString;
+        }
+
+        public sealed class PictureDispConverter
+        {
+            [DllImport("OleAut32.dll", EntryPoint = "OleCreatePictureIndirect", ExactSpelling = true, PreserveSig = false)]
+            private static extern stdole.IPictureDisp
+                OleCreatePictureIndirect([MarshalAs(UnmanagedType.AsAny)] object picdesc,ref Guid iid,[MarshalAs(UnmanagedType.Bool)] bool fOwn);
+            static Guid iPictureDispGuid = typeof(stdole.IPictureDisp).GUID;
+            private static class PICTDESC
+            {
+                //Picture Types
+                public const short PICTYPE_UNINITIALIZED = -1;
+                public const short PICTYPE_NONE = 0;
+                public const short PICTYPE_BITMAP = 1;
+                public const short PICTYPE_METAFILE = 2;
+                public const short PICTYPE_ICON = 3;
+                public const short PICTYPE_ENHMETAFILE = 4;
+                [StructLayout(LayoutKind.Sequential)]
+                public class Icon
+                {
+                    internal int cbSizeOfStruct = Marshal.SizeOf(typeof(PICTDESC.Icon));
+                    internal int picType = PICTDESC.PICTYPE_ICON;
+                    internal IntPtr hicon = IntPtr.Zero;
+                    internal int unused1;
+                    internal int unused2;
+                    internal Icon(System.Drawing.Icon icon)
+                    {
+                        this.hicon = icon.ToBitmap().GetHicon();
+                    }
+                }
+                [StructLayout(LayoutKind.Sequential)]
+                public class Bitmap
+                {
+                    internal int cbSizeOfStruct = Marshal.SizeOf(typeof(PICTDESC.Bitmap));
+                    internal int picType = PICTDESC.PICTYPE_BITMAP;
+                    internal IntPtr hbitmap = IntPtr.Zero;
+                    internal IntPtr hpal = IntPtr.Zero;
+                    internal int unused;
+                    internal Bitmap(System.Drawing.Bitmap bitmap)
+                    {
+                        this.hbitmap = bitmap.GetHbitmap();
+                    }
+                }
+            }
+
+            public static stdole.IPictureDisp ToIPictureDisp(
+                System.Drawing.Icon icon)
+            {
+                PICTDESC.Icon pictIcon = new PICTDESC.Icon(icon);
+                return OleCreatePictureIndirect(
+                    pictIcon, ref iPictureDispGuid, true);
+            }
+            public static stdole.IPictureDisp ToIPictureDisp(
+                System.Drawing.Bitmap bmp)
+            {
+                PICTDESC.Bitmap pictBmp = new PICTDESC.Bitmap(bmp);
+                return OleCreatePictureIndirect(pictBmp, ref iPictureDispGuid, true);
+            }
+        }
         #endregion
 
     }
---
+++ DrawingTools/invTools.cs
@@ -0,0 +1,177 @@
+﻿using Inventor;
+using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+//using System.Runtime.InteropServices;
+using System.Drawing;
+
+namespace invTools
+{
+
+    class PartDetails
+    {
+        Inventor.ApplicationAddInSite addInSiteObject = null;
+
+        public void ViewDetails()
+        {
+            Inventor.Application invApp = addInSiteObject.Application;
+            Inventor.Document invDoc = invApp.ActiveDocument;
+            Inventor.Point2d oPosition = null;
+
+            //Check if none selected
+            SelectSet oS = invDoc.SelectSet;
+
+            if (oS.Count == 0)
+            {
+                MessageBox.Show("Select a drawing view");
+                return;
+            }
+
+            //Reference to the drawing view from the 1st selected object
+            DrawingView oView = (DrawingView)oS[1];
+            Inventor.Document viewDoc = (Document)oView.ReferencedDocumentDescriptor.ReferencedDocument;
+            Property propTitle, propComments, propKeywords, propQty;
+            propTitle = invDoc.PropertySets["Summary Information"]["Title"];
+            propComments = invDoc.PropertySets["Summary Information"]["Comments"];
+            propKeywords = invDoc.PropertySets["Summary Information"]["Keywords"];
+            propQty = invDoc.PropertySets["User Defined Properties"]["Quantity"];
+
+            String valueTitle, valueKeywords, valueComments, valueQtyFinal;
+            int valueQty = 0;
+            valueTitle = (String)propTitle.Value;
+            valueKeywords = (String)propKeywords.Value;
+            valueComments = (String)propComments.Value;
+            try
+            {
+                valueQty = (int)propQty.Value;
+            }
+            catch
+            {
+                valueQty = 0;
+            }
+
+            // Check if Comments is empty then Comments = null
+            if (valueComments == "")
+            {
+                valueComments = "null";
+            }
+
+            // Check if Qty < 10 Then add "0" to first
+            if (valueQty < 10)
+            {
+                valueQtyFinal = "0" + Convert.ToString(valueQty);
+            }
+            else
+            {
+                valueQtyFinal = Convert.ToString(valueQty);
+            }
+
+            // Remove space in head text
+            if (Left(valueTitle, 1) == " ")
+            {
+                valueTitle = Right(valueTitle, valueTitle.Length - 1);
+            
+            }
+
+            // Content
+            String sFormattedText;
+            sFormattedText = "<StyleOverride FontSize = '0.28'><StyleOverride FontSize = '0.305' Underline = 'True'>" + "Chi tiết: " + valueTitle + "</StyleOverride>" +
+                "\r\n" + "Ký hiệu: " + valueKeywords + "\r\n" + "Số lượng: " + valueQtyFinal + "\r\n" + "Vật liệu: " + valueComments + "</StyleOverride>";
+            DrawingDocument drawDoc = (DrawingDocument)invApp.ActiveDocument;
+            Sheet oSheet = drawDoc.ActiveSheet;
+            GeneralNotes oGenNotes = oSheet.DrawingNotes.GeneralNotes;
+
+            // Check if keywords is empty then Get keywords from another GeneralNote
+            if(valueKeywords == "")
+            {
+                try
+                {
+                    GeneralNote oGenNoteSelected = (GeneralNote)invApp.CommandManager.Pick(SelectionFilterEnum.kDrawingNoteFilter, "Select DrawingNote");
+                    valueKeywords = GetTextFromNote(oGenNoteSelected.FormattedText, "Ký hiệu");
+                    if(valueKeywords == "")
+                    {
+                        valueKeywords = "null";
+                    }
+                    else
+                    {
+                        propKeywords.Value = valueKeywords;
+                        sFormattedText = "<StyleOverride FontSize = '0.28'><StyleOverride FontSize = '0.305' Underline = 'True'>" + "Chi tiết: " + valueTitle + "</StyleOverride>" +
+                "\r\n" + "Ký hiệu: " + valueKeywords + "\r\n" + "Số lượng: " + valueQtyFinal + "\r\n" + "Vật liệu: " + valueComments + "</StyleOverride>";
+                    }
+                    oGenNotes.AddFitted(oGenNoteSelected.Position, sFormattedText);
+                    oGenNoteSelected.Delete();
+                }
+                catch
+                {
+
+                }
+
+                // Check GeneralNote exist
+                bool CheckNGExist = false;
+                int CountExist = 0;
+                GeneralNote oGeneralNotTemp = null;
+                foreach (GeneralNote oGeneralNote in drawDoc.ActiveSheet.DrawingNotes.GeneralNotes)
+                {
+                    if (oGeneralNote.FormattedText.ToUpper().IndexOf(valueTitle.ToUpper()) != 0)
+                    {
+                        oPosition = oGeneralNote.Position;
+                        oGeneralNotTemp = oGeneralNote;
+                        CountExist++;
+                        CheckNGExist = true;
+                    }
+                }
+                if (CheckNGExist == true) // Trường hợp đã tồn tại GeneralNote trùng khớp
+                {
+                    if(CountExist == 1) // Nếu có 1 trường hợp trùng khớp => Tạo mới, xoá cũ
+                    {
+                        oGenNotes.AddFitted(oPosition, sFormattedText);
+                        oGeneralNotTemp.Delete();
+                    }
+                    else
+                    {
+                        GeneralNote oGenNoteSelected = (GeneralNote)invApp.CommandManager.Pick(SelectionFilterEnum.kDrawingNoteFilter, "Found " + CountExist + " results match. Pick one..");
+                        oGenNotes.AddFitted(oGenNoteSelected.Position, sFormattedText);
+                        oGenNoteSelected.Delete();
+                    }
+                }
+                else
+                {
+                    double h, D;
+                    h = oView.Height;
+                    D = oView.Width;
+                    TransientGeometry oTG = invApp.TransientGeometry;
+                    oGenNotes.AddFitted(oTG.CreatePoint2d(oView.Position.X - D / 6, oView.Position.Y + h / 2 + 4), sFormattedText);
+                }
+
+            }
+           
+        }
+
+        string GetTextFromNote(string oSource, string oFindText)
+        {
+            int oCheck, LenFindText, oStartPos, oEndPos;
+            oCheck = oSource.IndexOf(oFindText);
+            LenFindText = oFindText.Length;
+            if(oCheck != 0)
+            {
+                oStartPos = oCheck - 1;
+                oEndPos = oSource.IndexOf("<", oStartPos) - 1;
+                oSource = Left(oSource, oEndPos);
+                oSource = Right(oSource, oSource.Length - oStartPos);
+                oSource = Right(oSource, oSource.Length - LenFindText - 2);
+            }
+            return oSource;
+        }
+
+        string Left(string input, int count)
+        {
+            return input.Substring(0, Math.Min(input.Length, count));
+        }
+
+        string Right(string input, int count)
+        {
+            return input.Substring(Math.Max(input.Length - count, 0), Math.Min(count, input.Length));
+        }
+    }
+
+}
