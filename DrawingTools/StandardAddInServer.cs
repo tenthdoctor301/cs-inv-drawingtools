40be39298ee0cc4c50e1280e3ba65ac4245cec00
tenthdoctor301 <139527335+tenthdoctor301@users.noreply.github.com>
2023-08-11 17:03:04 +0700

    Fix 11082023

--- DrawingTools/StandardAddInServer.cs
+++ DrawingTools/StandardAddInServer.cs
@@ -19,11 +19,11 @@ namespace DrawingTools
     {
 
         // Inventor application object.
-        Inventor.Application m_inventorApplication;
-        ButtonDefinition CreateDetailsButton;
-        //UserInterfaceEvents m_uiEvents;
-        //ApplicationEvents m_appEvents;
-        private ButtonDefinitionSink_OnExecuteEventHandler ButtonDefinition_OnExecuteEventDelegate;
+        private Inventor.Application invApp;
+        private PartDetails partDetails;
+        
+
+        public ButtonDefinitionSink_OnExecuteEventHandler ButtonDefinition_OnExecuteEventDelegate;
         public StandardAddInServer()
         {
         }
@@ -31,33 +31,23 @@ namespace DrawingTools
         #region ApplicationAddInServer Members
         public void Activate(Inventor.ApplicationAddInSite addInSiteObject, bool firstTime)
         {
-            // This method is called by Inventor when it loads the addin.
-            // The AddInSiteObject provides access to the Inventor Application object.
-            // The FirstTime flag indicates if the addin is loaded for the first time.
-
-            // Initialize AddIn members.
-            m_inventorApplication = addInSiteObject.Application;
-            //m_uiEvents = m_inventorApplication.UserInterfaceManager.UserInterfaceEvents;
-            //m_appEvents = m_inventorApplication.ApplicationEvents;
-            
-            // creating a button definition.
-            stdole.IPictureDisp largeIcon = PictureDispConverter.ToIPictureDisp(InvAddIn.Properties.Resources._32x32);
-            stdole.IPictureDisp smallIcon = PictureDispConverter.ToIPictureDisp(InvAddIn.Properties.Resources._16x16);
-            ControlDefinitions controlDefs = m_inventorApplication.CommandManager.ControlDefinitions;
-            CreateDetailsButton = controlDefs.AddButtonDefinition("View Details", "id_viewdetails_bt", CommandTypesEnum.kShapeEditCmdType, AddInClientID(), "", "", smallIcon, largeIcon);
-            ButtonDefinition_OnExecuteEventDelegate = new ButtonDefinitionSink_OnExecuteEventHandler(this.CreateDetailsButton_OnExecute);
-            CreateDetailsButton.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
-
+            invApp = addInSiteObject.Application;
+            partDetails = new PartDetails();
+            partDetails.OnExecute(invApp, AddInClientID());
             // Add to the user interface, if it's the first time.
+            
             if (firstTime == true)
             {
-                AddToUserInterface();
+
+                partDetails.AddToUserInterface(AddInClientID());
             }
-            
+
             // TODO: Add ApplicationAddInServer.Activate implementation.
             // e.g. event initialization, command creation etc.
         }
-        
+
+       
+
         public void Deactivate()
         {
             // This method is called by Inventor when the AddIn is unloaded.
@@ -67,7 +57,7 @@ namespace DrawingTools
             // TODO: Add ApplicationAddInServer.Deactivate implementation
 
             // Release objects.
-            m_inventorApplication = null;
+            invApp = null;
 
             GC.Collect();
             GC.WaitForPendingFinalizers();
@@ -92,27 +82,11 @@ namespace DrawingTools
                 return null;
             }
         }
+       // private invTools.PartDetails m_PartDetails;
        
-        public void CreateDetailsButton_OnExecute(NameValueMap context)
-        {
-            invTools.PartDetails r = new invTools.PartDetails();
-            r.ViewDetails();
-            
-        }
 
-        private void AddToUserInterface()
-        {
-            RibbonPanel panel;
-            try
-            {
-                panel = m_inventorApplication.UserInterfaceManager.Ribbons["Drawing"].RibbonTabs["id_TabPlaceViews"].RibbonPanels["id_UserToolTab"];
-            }
-            catch
-            {
-                panel = m_inventorApplication.UserInterfaceManager.Ribbons["Drawing"].RibbonTabs["id_TabPlaceViews"].RibbonPanels.Add("Tool Panel", "id_UserToolTab", AddInClientID(), "",false);
-            }
-            panel.CommandControls.AddButton(CreateDetailsButton);
-        }
+
+
 
         public string AddInClientID()
         {
@@ -123,63 +97,7 @@ namespace DrawingTools
             return addInCLSIDString;
         }
 
-        public sealed class PictureDispConverter
-        {
-            [DllImport("OleAut32.dll", EntryPoint = "OleCreatePictureIndirect", ExactSpelling = true, PreserveSig = false)]
-            private static extern stdole.IPictureDisp
-                OleCreatePictureIndirect([MarshalAs(UnmanagedType.AsAny)] object picdesc,ref Guid iid,[MarshalAs(UnmanagedType.Bool)] bool fOwn);
-            static Guid iPictureDispGuid = typeof(stdole.IPictureDisp).GUID;
-            private static class PICTDESC
-            {
-                //Picture Types
-                public const short PICTYPE_UNINITIALIZED = -1;
-                public const short PICTYPE_NONE = 0;
-                public const short PICTYPE_BITMAP = 1;
-                public const short PICTYPE_METAFILE = 2;
-                public const short PICTYPE_ICON = 3;
-                public const short PICTYPE_ENHMETAFILE = 4;
-                [StructLayout(LayoutKind.Sequential)]
-                public class Icon
-                {
-                    internal int cbSizeOfStruct = Marshal.SizeOf(typeof(PICTDESC.Icon));
-                    internal int picType = PICTDESC.PICTYPE_ICON;
-                    internal IntPtr hicon = IntPtr.Zero;
-                    internal int unused1;
-                    internal int unused2;
-                    internal Icon(System.Drawing.Icon icon)
-                    {
-                        this.hicon = icon.ToBitmap().GetHicon();
-                    }
-                }
-                [StructLayout(LayoutKind.Sequential)]
-                public class Bitmap
-                {
-                    internal int cbSizeOfStruct = Marshal.SizeOf(typeof(PICTDESC.Bitmap));
-                    internal int picType = PICTDESC.PICTYPE_BITMAP;
-                    internal IntPtr hbitmap = IntPtr.Zero;
-                    internal IntPtr hpal = IntPtr.Zero;
-                    internal int unused;
-                    internal Bitmap(System.Drawing.Bitmap bitmap)
-                    {
-                        this.hbitmap = bitmap.GetHbitmap();
-                    }
-                }
-            }
 
-            public static stdole.IPictureDisp ToIPictureDisp(
-                System.Drawing.Icon icon)
-            {
-                PICTDESC.Icon pictIcon = new PICTDESC.Icon(icon);
-                return OleCreatePictureIndirect(
-                    pictIcon, ref iPictureDispGuid, true);
-            }
-            public static stdole.IPictureDisp ToIPictureDisp(
-                System.Drawing.Bitmap bmp)
-            {
-                PICTDESC.Bitmap pictBmp = new PICTDESC.Bitmap(bmp);
-                return OleCreatePictureIndirect(pictBmp, ref iPictureDispGuid, true);
-            }
-        }
         #endregion
 
     }
--- DrawingTools/invTools.cs
+++ DrawingTools/invTools.cs
@@ -2,53 +2,72 @@
 using Microsoft.Win32;
 using System;
 using System.Windows.Forms;
-//using System.Runtime.InteropServices;
+using System.Runtime.InteropServices;
 using System.Drawing;
 
-namespace invTools
+namespace DrawingTools
 {
 
     class PartDetails
     {
-        Inventor.ApplicationAddInSite addInSiteObject = null;
 
-        public void ViewDetails()
+        public ButtonDefinitionSink_OnExecuteEventHandler ButtonDefinition_OnExecuteEventDelegate;
+        ButtonDefinition CreateDetailsButton;
+        private Inventor.Application invApp;
+
+        public void OnExecute(Inventor.Application invApp, string AddInClientID)
         {
-            Inventor.Application invApp = addInSiteObject.Application;
-            Inventor.Document invDoc = invApp.ActiveDocument;
-            Inventor.Point2d oPosition = null;
+            this.invApp = invApp;
+            Document oDoc = invApp.ActiveDocument;
+            stdole.IPictureDisp largeIcon = PictureDispConverter.ToIPictureDisp(InvAddIn.Properties.Resources._32x32);
+            stdole.IPictureDisp smallIcon = PictureDispConverter.ToIPictureDisp(InvAddIn.Properties.Resources._16x16);
+            ControlDefinitions controlDefs = invApp.CommandManager.ControlDefinitions;
+            CreateDetailsButton = controlDefs.AddButtonDefinition("View Details", "id_viewdetails_bt", CommandTypesEnum.kShapeEditCmdType, AddInClientID, "", "", smallIcon, largeIcon);
+            ButtonDefinition_OnExecuteEventDelegate = new ButtonDefinitionSink_OnExecuteEventHandler(this.ViewDetails);
+            CreateDetailsButton.OnExecute += ButtonDefinition_OnExecuteEventDelegate;
 
+        }
+
+        public void ViewDetails(NameValueMap context)
+        {
+            
+            
+            Document invDoc = invApp.ActiveDocument;
+            
+            Point2d oPosition = null;
+           
             //Check if none selected
             SelectSet oS = invDoc.SelectSet;
-
+            
             if (oS.Count == 0)
             {
                 MessageBox.Show("Select a drawing view");
                 return;
             }
-
+            
             //Reference to the drawing view from the 1st selected object
             DrawingView oView = (DrawingView)oS[1];
             Inventor.Document viewDoc = (Document)oView.ReferencedDocumentDescriptor.ReferencedDocument;
             Property propTitle, propComments, propKeywords, propQty;
-            propTitle = invDoc.PropertySets["Summary Information"]["Title"];
-            propComments = invDoc.PropertySets["Summary Information"]["Comments"];
-            propKeywords = invDoc.PropertySets["Summary Information"]["Keywords"];
-            propQty = invDoc.PropertySets["User Defined Properties"]["Quantity"];
-
             String valueTitle, valueKeywords, valueComments, valueQtyFinal;
-            int valueQty = 0;
-            valueTitle = (String)propTitle.Value;
-            valueKeywords = (String)propKeywords.Value;
-            valueComments = (String)propComments.Value;
+            int valueQty;
+            propTitle = viewDoc.PropertySets["Summary Information"]["Title"];
+            
+            propComments = viewDoc.PropertySets["Summary Information"]["Comments"];
+            propKeywords = viewDoc.PropertySets["Summary Information"]["Keywords"];
             try
             {
+                propQty = viewDoc.PropertySets["User Defined Properties"]["Quantity"];
                 valueQty = (int)propQty.Value;
             }
             catch
             {
                 valueQty = 0;
             }
+
+            valueTitle = (String)propTitle.Value;
+            valueKeywords = (String)propKeywords.Value;
+            valueComments = (String)propComments.Value;
 
             // Check if Comments is empty then Comments = null
             if (valueComments == "")
@@ -171,6 +190,78 @@ namespace invTools
         string Right(string input, int count)
         {
             return input.Substring(Math.Max(input.Length - count, 0), Math.Min(count, input.Length));
+        }
+
+        public void AddToUserInterface(string AddInClientID)
+        {
+            RibbonPanel panel;
+            try
+            {
+                panel = invApp.UserInterfaceManager.Ribbons["Drawing"].RibbonTabs["id_TabPlaceViews"].RibbonPanels["id_UserToolTab"];
+            }
+            catch
+            {
+                panel = invApp.UserInterfaceManager.Ribbons["Drawing"].RibbonTabs["id_TabPlaceViews"].RibbonPanels.Add("Tool Panel", "id_UserToolTab", AddInClientID, "", false);
+            }
+            panel.CommandControls.AddButton(CreateDetailsButton);
+        }
+
+        public sealed class PictureDispConverter
+        {
+            [DllImport("OleAut32.dll", EntryPoint = "OleCreatePictureIndirect", ExactSpelling = true, PreserveSig = false)]
+            private static extern stdole.IPictureDisp
+                OleCreatePictureIndirect([MarshalAs(UnmanagedType.AsAny)] object picdesc, ref Guid iid, [MarshalAs(UnmanagedType.Bool)] bool fOwn);
+            static Guid iPictureDispGuid = typeof(stdole.IPictureDisp).GUID;
+            private static class PICTDESC
+            {
+                //Picture Types
+                public const short PICTYPE_UNINITIALIZED = -1;
+                public const short PICTYPE_NONE = 0;
+                public const short PICTYPE_BITMAP = 1;
+                public const short PICTYPE_METAFILE = 2;
+                public const short PICTYPE_ICON = 3;
+                public const short PICTYPE_ENHMETAFILE = 4;
+                [StructLayout(LayoutKind.Sequential)]
+                public class Icon
+                {
+                    internal int cbSizeOfStruct = Marshal.SizeOf(typeof(PICTDESC.Icon));
+                    internal int picType = PICTDESC.PICTYPE_ICON;
+                    internal IntPtr hicon = IntPtr.Zero;
+                    internal int unused1;
+                    internal int unused2;
+                    internal Icon(System.Drawing.Icon icon)
+                    {
+                        this.hicon = icon.ToBitmap().GetHicon();
+                    }
+                }
+                [StructLayout(LayoutKind.Sequential)]
+                public class Bitmap
+                {
+                    internal int cbSizeOfStruct = Marshal.SizeOf(typeof(PICTDESC.Bitmap));
+                    internal int picType = PICTDESC.PICTYPE_BITMAP;
+                    internal IntPtr hbitmap = IntPtr.Zero;
+                    internal IntPtr hpal = IntPtr.Zero;
+                    internal int unused;
+                    internal Bitmap(System.Drawing.Bitmap bitmap)
+                    {
+                        this.hbitmap = bitmap.GetHbitmap();
+                    }
+                }
+            }
+
+            public static stdole.IPictureDisp ToIPictureDisp(
+                System.Drawing.Icon icon)
+            {
+                PICTDESC.Icon pictIcon = new PICTDESC.Icon(icon);
+                return OleCreatePictureIndirect(
+                    pictIcon, ref iPictureDispGuid, true);
+            }
+            public static stdole.IPictureDisp ToIPictureDisp(
+                System.Drawing.Bitmap bmp)
+            {
+                PICTDESC.Bitmap pictBmp = new PICTDESC.Bitmap(bmp);
+                return OleCreatePictureIndirect(pictBmp, ref iPictureDispGuid, true);
+            }
         }
     }
 
